1d6b78d56fe61f0f4ef457101d09230a24745da5
Gledson A. <g_a6@live.com>
2024-04-08 23:42:24 -0300

    Adding custom _app to add the query client provider from the react-query lib

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+    },
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
