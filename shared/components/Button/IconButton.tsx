3e6e23213f224d5ec3be8bb0f6b9d76d6f7210ed
Gledson A. <g_a6@live.com>
2024-04-11 01:10:21 -0300

    Importing global CSS variables for the color palette in the project from the global file

--- shared/components/Button/IconButton.tsx
+++ shared/components/Button/IconButton.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./styles.module.css";
+import styles from "@shared/components/Button/styles.module.css";
 
 type ButtonProps = {
   imageSourcePath: string;
