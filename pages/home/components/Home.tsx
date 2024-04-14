afcde391c902a5287c6f3a5cc8ae25e5f4956e0e
Gledson A. <g_a6@live.com>
2024-04-14 16:39:17 -0300

    Changing the home page to use just a logo and a text

--- pages/home/components/Home.tsx
+++ pages/home/components/Home.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
-import backgroundImage from "@public/dog-reading-3.jpg"
-import "@pages/home/components/styles.css"
+import "@pages/home/components/styles.css";
+import bookLogo from "@public/book.svg";
+import Logo from "@shared/components/Logo/Logo";
 
 export default function Home() {
   return (
     <>
-      <div className="image-container">
-        <Image src={backgroundImage} alt="home page background" width={5957} height={3493}></Image>
-        <div className="main-text">Feeling like reading something?</div>
-      </div>
+      <Logo
+        imageSourcePath={bookLogo}
+        altText="Logo"
+      />
+      <div className="main-text">Feeling like reading something?</div>
     </>
   );
 }
--- shared/components/Logo/Logo.tsx
+++ shared/components/Logo/Logo.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 type LogoParams = {
   imageSourcePath: string;
   altText: string;
-  imageWidth: number;
-  imageHeight: number;
+  imageWidth?: number;
+  imageHeight?: number;
 };
 
 export default function Logo({
   imageSourcePath,
   altText,
-  imageWidth,
-  imageHeight,
+  imageWidth = 1,
+  imageHeight = 1,
 }: LogoParams) {
   return (
-    <div>
+    <div className="image-container">
       <Image src={imageSourcePath} alt={altText} width={imageWidth} height={imageHeight}></Image>
     </div>
   );
